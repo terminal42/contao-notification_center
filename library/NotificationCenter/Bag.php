f453650c47a8974a3b135a7b84eae4bb57f05558
Yanick Witschi <yanick.witschi@terminal42.ch>
2013-10-04 15:31:05 +0200

    Added Bag class that will be responsible for sending a notification bag

---
+++ library/NotificationCenter/Bag.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  terminal42 gmbh 2013
+ * @license    LGPL
+ */
+
+namespace NotificationCenter;
+
+use NotificationCenter\Model\Bag as BagModel;
+use NotificationCenter\Model\Notification;
+
+class Bag
+{
+    /**
+     * Sends a notification bag
+     * @param   int The notification bag ID
+     * @param   array The tokens
+     * @param   string The language (optional)
+     * @return  boolean
+     */
+    public static function send($intBagId, $arrTokens, $strLanguage='')
+    {
+        // Check if this is a valid Bag model id
+        if (($objBag = BagModel::findByPk($intBagId)) === null) {
+            return false;
+        }
+
+        // Check if there are valid notifications
+        if (($objNotifications = $objBag->getNotifications()) === null) {
+            return false;
+        }
+
+        // Check if there is a valid bag type
+        if (($objBagType = $objBag->buildBagType()) === null) {
+            return false;
+        }
+
+        // Set default language
+        if ($strLanguage === '') {
+            $strLanguage = $GLOBALS['TL_LANGUAGE'];
+        }
+
+        $blnHasError = false;
+
+        while ($objNotifications->next()) {
+            $objNotification = $objNotifications->current();
+
+            if (($objGateway = $objNotification->buildGateway($objBagType, $strLanguage)) == null) {
+                $blnHasError = true;
+            }
+
+            $objGateway->send($arrTokens);
+        }
+
+        return $blnHasError;
+    }
+}
