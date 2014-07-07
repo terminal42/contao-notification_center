39346d1d6718e6281ebbafbb4ceb95dda4a09e51
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-07-07 11:24:30 +0200

    Language files

---
+++ languages/en/tl_nc_queue.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  terminal42 gmbh 2014
+ * @license    LGPL
+ */
+
+/**
+ * Status
+ */
+$GLOBALS['TL_LANG']['tl_nc_queue']['status']['queued']                       = 'Waiting in queue.';
+$GLOBALS['TL_LANG']['tl_nc_queue']['status']['error']                        = 'Error sending the message. Check the system log for more details.';
+$GLOBALS['TL_LANG']['tl_nc_queue']['status']['sent']                         = 'Message sent.';
+$GLOBALS['TL_LANG']['tl_nc_queue']['source']                                 = 'Source';
