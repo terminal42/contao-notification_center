4949e838cf0ba36973595081e57cb0af07248d8c
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-08-11 21:58:03 +0200

    Support friendly recipient names

--- library/NotificationCenter/Util/String.php
+++ library/NotificationCenter/Util/String.php
@@ -159,6 +159,8 @@ class String extends \Controller
             if ($strAddress != '') {
                 $strAddress = static::recursiveReplaceTokensAndTags($strAddress, $arrTokens, static::NO_TAGS|static::NO_BREAKS);
 
+                list ($strName, $strAddress) = \String::splitFriendlyEmail($strAddress);
+
                 // Address could become empty through invalid insert tag
                 if ($strAddress == '' || !\Validator::isEmail($strAddress)) {
                     continue;
