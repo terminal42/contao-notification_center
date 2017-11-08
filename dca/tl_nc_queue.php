68fb10cacb984f7ce4427c91aeb7ed7a6791b592
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-11-08 10:36:49 +0100

    Added DCA fields for delayed sending

--- dca/tl_nc_queue.php
+++ dca/tl_nc_queue.php
@@ -112,6 +112,12 @@ $GLOBALS['TL_DCA']['tl_nc_queue'] = array
             'flag'                    => 6,
             'sql'                     => "int(10) unsigned NOT NULL default '0'"
         ),
+        'dateDelay' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_nc_queue']['dateDelay'],
+            'flag'                    => 6,
+            'sql'                     => "int(10) unsigned NOT NULL default '0'"
+        ),
         'dateSent' => array
         (
             'label'                   => &$GLOBALS['TL_LANG']['tl_nc_queue']['dateSent'],
