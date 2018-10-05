91844b0ff616a8cd24b15356ffb696e1e9e11bb3
David Molineus <david.molineus@netzmacht.de>
2018-10-05 10:43:47 +0200

    Indent by spaces.

--- modules/ModuleNewsletterUnsubscribeNotificationCenter.php
+++ modules/ModuleNewsletterUnsubscribeNotificationCenter.php
@@ -24,116 +24,116 @@ class ModuleNewsletterUnsubscribeNotificationCenter extends ModuleUnsubscribe
 {
     use NewsletterModuleTrait;
 
-	/**
-	 * Generate the module
-	 */
-	protected function compile()
-	{
-	    $this->setCustomTemplate();
+    /**
+     * Generate the module
+     */
+    protected function compile()
+    {
+        $this->setCustomTemplate();
 
-		$this->Template->email = '';
-		$this->Template->captcha = '';
+        $this->Template->email = '';
+        $this->Template->captcha = '';
 
-		$objCaptchaWidget = $this->createCaptchaWidgetIfEnabled();
+        $objCaptchaWidget = $this->createCaptchaWidgetIfEnabled();
 
-		$strFormId = 'tl_unsubscribe_' . $this->id;
+        $strFormId = 'tl_unsubscribe_' . $this->id;
 
-		$this->processForm($strFormId, $objCaptchaWidget, 'removeRecipient');
+        $this->processForm($strFormId, $objCaptchaWidget, 'removeRecipient');
 
-		// Add the captcha widget to the template
-		if ($objCaptchaWidget !== null)
-		{
-			$this->Template->captcha = $objCaptchaWidget->parse();
-		}
+        // Add the captcha widget to the template
+        if ($objCaptchaWidget !== null)
+        {
+            $this->Template->captcha = $objCaptchaWidget->parse();
+        }
 
-		$session = \System::getContainer()->get('session');
-		$flashBag = $session->getFlashBag();
+        $session = \System::getContainer()->get('session');
+        $flashBag = $session->getFlashBag();
 
-		// Confirmation message
-		if ($session->isStarted() && $flashBag->has('nl_removed'))
-		{
-			$arrMessages = $flashBag->get('nl_removed');
+        // Confirmation message
+        if ($session->isStarted() && $flashBag->has('nl_removed'))
+        {
+            $arrMessages = $flashBag->get('nl_removed');
 
-			$this->Template->mclass = 'confirm';
-			$this->Template->message = $arrMessages[0];
-		}
+            $this->Template->mclass = 'confirm';
+            $this->Template->message = $arrMessages[0];
+        }
 
-		// Default template variables
-		$this->Template->channels = $this->compileChannels();
-		$this->Template->showChannels = !$this->nl_hideChannels;
-		$this->Template->email = \Input::get('email');
-		$this->Template->submit = \StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['unsubscribe']);
-		$this->Template->channelsLabel = $GLOBALS['TL_LANG']['MSC']['nl_channels'];
-		$this->Template->emailLabel = $GLOBALS['TL_LANG']['MSC']['emailAddress'];
-		$this->Template->action = \Environment::get('indexFreeRequest');
-		$this->Template->formId = $strFormId;
-		$this->Template->id = $this->id;
-	}
+        // Default template variables
+        $this->Template->channels = $this->compileChannels();
+        $this->Template->showChannels = !$this->nl_hideChannels;
+        $this->Template->email = \Input::get('email');
+        $this->Template->submit = \StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['unsubscribe']);
+        $this->Template->channelsLabel = $GLOBALS['TL_LANG']['MSC']['nl_channels'];
+        $this->Template->emailLabel = $GLOBALS['TL_LANG']['MSC']['emailAddress'];
+        $this->Template->action = \Environment::get('indexFreeRequest');
+        $this->Template->formId = $strFormId;
+        $this->Template->id = $this->id;
+    }
 
-	/**
-	 * Remove the recipient
-	 *
-	 * @param string $strEmail
-	 * @param array  $arrRemove
-	 */
-	protected function removeRecipient($strEmail, $arrRemove)
-	{
-		// Remove the subscriptions
-		if (($objRemove = \NewsletterRecipientsModel::findByEmailAndPids($strEmail, $arrRemove)) !== null)
-		{
-			while ($objRemove->next())
-			{
-				$strHash = md5($objRemove->email);
+    /**
+     * Remove the recipient
+     *
+     * @param string $strEmail
+     * @param array  $arrRemove
+     */
+    protected function removeRecipient($strEmail, $arrRemove)
+    {
+        // Remove the subscriptions
+        if (($objRemove = \NewsletterRecipientsModel::findByEmailAndPids($strEmail, $arrRemove)) !== null)
+        {
+            while ($objRemove->next())
+            {
+                $strHash = md5($objRemove->email);
 
-				// Add a blacklist entry (see #4999)
-				if (($objBlacklist = \NewsletterBlacklistModel::findByHashAndPid($strHash, $objRemove->pid)) === null)
-				{
-					$objBlacklist = new \NewsletterBlacklistModel();
-					$objBlacklist->pid = $objRemove->pid;
-					$objBlacklist->hash = $strHash;
-					$objBlacklist->save();
-				}
+                // Add a blacklist entry (see #4999)
+                if (($objBlacklist = \NewsletterBlacklistModel::findByHashAndPid($strHash, $objRemove->pid)) === null)
+                {
+                    $objBlacklist = new \NewsletterBlacklistModel();
+                    $objBlacklist->pid = $objRemove->pid;
+                    $objBlacklist->hash = $strHash;
+                    $objBlacklist->save();
+                }
 
-				$objRemove->delete();
-			}
-		}
+                $objRemove->delete();
+            }
+        }
 
-		// Get the channels
-		$objChannels = \NewsletterChannelModel::findByIds($arrRemove);
-		$arrChannels = $objChannels->fetchEach('title');
+        // Get the channels
+        $objChannels = \NewsletterChannelModel::findByIds($arrRemove);
+        $arrChannels = $objChannels->fetchEach('title');
 
-		// HOOK: post unsubscribe callback
-		if (isset($GLOBALS['TL_HOOKS']['removeRecipient']) && \is_array($GLOBALS['TL_HOOKS']['removeRecipient']))
-		{
-			foreach ($GLOBALS['TL_HOOKS']['removeRecipient'] as $callback)
-			{
-				$this->import($callback[0]);
-				$this->{$callback[0]}->{$callback[1]}($strEmail, $arrRemove);
-			}
-		}
+        // HOOK: post unsubscribe callback
+        if (isset($GLOBALS['TL_HOOKS']['removeRecipient']) && \is_array($GLOBALS['TL_HOOKS']['removeRecipient']))
+        {
+            foreach ($GLOBALS['TL_HOOKS']['removeRecipient'] as $callback)
+            {
+                $this->import($callback[0]);
+                $this->{$callback[0]}->{$callback[1]}($strEmail, $arrRemove);
+            }
+        }
 
-		// Prepare the simple token data
-		$arrData = array();
-		$arrData['domain'] = \Idna::decode(\Environment::get('host'));
-		$arrData['channel'] = $arrData['channels'] = implode("\n", $arrChannels);
+        // Prepare the simple token data
+        $arrData = array();
+        $arrData['domain'] = \Idna::decode(\Environment::get('host'));
+        $arrData['channel'] = $arrData['channels'] = implode("\n", $arrChannels);
 
-		// Confirmation e-mail
-		$objEmail = new \Email();
-		$objEmail->from = $GLOBALS['TL_ADMIN_EMAIL'];
-		$objEmail->fromName = $GLOBALS['TL_ADMIN_NAME'];
-		$objEmail->subject = sprintf($GLOBALS['TL_LANG']['MSC']['nl_subject'], \Idna::decode(\Environment::get('host')));
-		$objEmail->text = \StringUtil::parseSimpleTokens($this->nl_unsubscribe, $arrData);
-		$objEmail->sendTo($strEmail);
+        // Confirmation e-mail
+        $objEmail = new \Email();
+        $objEmail->from = $GLOBALS['TL_ADMIN_EMAIL'];
+        $objEmail->fromName = $GLOBALS['TL_ADMIN_NAME'];
+        $objEmail->subject = sprintf($GLOBALS['TL_LANG']['MSC']['nl_subject'], \Idna::decode(\Environment::get('host')));
+        $objEmail->text = \StringUtil::parseSimpleTokens($this->nl_unsubscribe, $arrData);
+        $objEmail->sendTo($strEmail);
 
-		// Redirect to the jumpTo page
-		if ($this->jumpTo && ($objTarget = $this->objModel->getRelated('jumpTo')) instanceof PageModel)
-		{
-			/** @var PageModel $objTarget */
-			$this->redirect($objTarget->getFrontendUrl());
-		}
+        // Redirect to the jumpTo page
+        if ($this->jumpTo && ($objTarget = $this->objModel->getRelated('jumpTo')) instanceof PageModel)
+        {
+            /** @var PageModel $objTarget */
+            $this->redirect($objTarget->getFrontendUrl());
+        }
 
-		\System::getContainer()->get('session')->getFlashBag()->set('nl_removed', $GLOBALS['TL_LANG']['MSC']['nl_removed']);
+        \System::getContainer()->get('session')->getFlashBag()->set('nl_removed', $GLOBALS['TL_LANG']['MSC']['nl_removed']);
 
-		$this->reload();
-	}
+        $this->reload();
+    }
 }
