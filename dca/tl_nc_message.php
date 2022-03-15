d7127259d8d21287da94677c969153efb5abe77e
Ralf Baussenwein <r.baussenwein@eikona-media.de>
2022-03-15 16:46:27 +0100

    Fix undefined key deleteConfirm

--- dca/tl_nc_message.php
+++ dca/tl_nc_message.php
@@ -90,7 +90,7 @@ $GLOBALS['TL_DCA']['tl_nc_message'] = array
                 'label'               => &$GLOBALS['TL_LANG']['tl_nc_message']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.gif',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ),
             'toggle' => array
             (
