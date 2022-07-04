805339bf89deca05bd64a3df494f32aff063599b
Ralf Baussenwein <r.baussenwein@eikona-media.de>
2022-07-04 12:16:15 +0200

    Insure to use correct format for framework language + locale

--- library/NotificationCenter/Model/Message.php
+++ library/NotificationCenter/Model/Message.php
@@ -76,7 +76,7 @@ class Message extends \Model
         if (null !== ($objLanguage = Language::findByMessageAndLanguageOrFallback($this, $cpLanguage))) {
              // Switch to the language of the notification
             $this->saveCurrentFrameworkLanguage();
-            $this->setFrameworkLanguage($objLanguage->language, str_replace('-', '_', $objLanguage->language));
+            $this->setFrameworkLanguage($objLanguage->language, $objLanguage->language);
         }
 
         $objGateway = $objGatewayModel->getGateway();
@@ -159,6 +159,9 @@ class Message extends \Model
         if (!$language || !$locale) {
             return;
         }
+
+        $language = str_replace('_', '-', $language);
+        $locale = str_replace('-', '_', $locale);
 
         $GLOBALS['TL_LANGUAGE'] = $language;
         \Contao\System::getContainer()->get('translator')->setLocale($locale);
