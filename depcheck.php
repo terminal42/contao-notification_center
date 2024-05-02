12030586f4f4b58f41d45fa6e63003e37f8e40fe
Yanick Witschi <yanick.witschi@terminal42.ch>
2024-05-02 13:55:45 +0200

    Ensure the correct dependencies are set on all services used

---
+++ depcheck.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->ignoreUnknownClasses([
+        'Contao\ModulePassword', // This class exists in Contao 4.13 but not in 5.3
+    ])
+    ->ignoreErrorsOnPackage('contao/newsletter-bundle', [ErrorType::DEV_DEPENDENCY_IN_PROD]) // This is an optional integration
+    ->ignoreErrorsOnPackage('terminal42/dcawizard', [ErrorType::UNUSED_DEPENDENCY]) // This is a widget used in the back end but not inside code
+;
