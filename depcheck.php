5f0d35a6306029f85fd1e379a248410b20ba5459
fritzmg <fmg@inspiredminds.at>
2024-05-24 08:49:28 +0100

    ignore error on package

--- depcheck.php
+++ depcheck.php
@@ -10,5 +10,6 @@ return (new Configuration())
         'Contao\ModulePassword', // This class exists in Contao 4.13 but not in 5.3
     ])
     ->ignoreErrorsOnPackage('contao/newsletter-bundle', [ErrorType::DEV_DEPENDENCY_IN_PROD]) // This is an optional integration
+    ->ignoreErrorsOnPackage('psr/log', [ErrorType::SHADOW_DEPENDENCY]) // Logging is optional
     ->ignoreErrorsOnPackage('terminal42/dcawizard', [ErrorType::UNUSED_DEPENDENCY]) // This is a widget used in the back end but not inside code
 ;
