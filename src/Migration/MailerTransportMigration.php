7c5949d5fd5f23ec19b66028ab653ad9b77762dd
Yanick Witschi <yanick.witschi@terminal42.ch>
2024-06-26 11:04:05 +0200

    Added mailer transport migration when upgrading from v1

---
+++ src/Migration/MailerTransportMigration.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Terminal42\NotificationCenterBundle\Migration;
+
+use Contao\CoreBundle\Migration\AbstractMigration;
+use Contao\CoreBundle\Migration\MigrationResult;
+use Doctrine\DBAL\Connection;
+
+class MailerTransportMigration extends AbstractMigration
+{
+    public function __construct(private readonly Connection $connection)
+    {
+    }
+
+    public function shouldRun(): bool
+    {
+        $schemaManager = $this->connection->createSchemaManager();
+
+        if (!$schemaManager->tablesExist(['tl_nc_gateway'])) {
+            return false;
+        }
+
+        $columns = $schemaManager->listTableColumns('tl_nc_gateway');
+
+        if (!isset($columns['mailertransport'])) {
+            return false;
+        }
+
+        return $this->connection->fetchOne("SELECT COUNT(*) FROM tl_nc_gateway WHERE mailerTransport=''") > 0;
+    }
+
+    public function run(): MigrationResult
+    {
+        $this->connection->executeStatement("UPDATE tl_nc_gateway SET mailerTransport = NULL WHERE mailerTransport = ''");
+
+        return $this->createResult(true);
+    }
+}
