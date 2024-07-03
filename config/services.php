86388e4389d16877a0779b9507c1f34b8434c4cc
Yanick Witschi <yanick.witschi@terminal42.ch>
2024-07-03 11:19:02 +0200

    Extracted the FileUploadNormalizer service (#346)

--- config/services.php
+++ config/services.php
@@ -17,7 +17,6 @@ use Terminal42\NotificationCenterBundle\NotificationType\NotificationTypeRegistr
 use Terminal42\NotificationCenterBundle\Token\Definition\Factory\ChainTokenDefinitionFactory;
 use Terminal42\NotificationCenterBundle\Token\Definition\Factory\CoreTokenDefinitionFactory;
 use Terminal42\NotificationCenterBundle\Token\Definition\Factory\TokenDefinitionFactoryInterface;
-use Terminal42\NotificationCenterBundle\Util\FileUploadNormalizer;
 
 return static function (ContainerConfigurator $container): void {
     $services = $container->services();
@@ -77,14 +76,6 @@ return static function (ContainerConfigurator $container): void {
             service(BulkyItemStorage::class),
             service(StringParser::class),
             service('translation.locale_switcher')->nullOnInvalid(),
-        ])
-    ;
-
-    $services->set(FileUploadNormalizer::class)
-        ->args([
-            param('kernel.project_dir'),
-            service('mime_types'),
-            service('contao.filesystem.virtual.files'),
         ])
     ;
 };
