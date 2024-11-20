ed1289740df3a63fd7d985fa959ea9a84c29428d
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2024-11-20 11:22:29 +0000

    Add the missing MailerTransportMigration service definition (#376)

--- config/migrations.php
+++ config/migrations.php
@@ -5,12 +5,19 @@ declare(strict_types=1);
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Terminal42\NotificationCenterBundle\Migration\EmailGatewayMigration;
+use Terminal42\NotificationCenterBundle\Migration\MailerTransportMigration;
 
 return static function (ContainerConfigurator $container): void {
     $services = $container->services();
     $services->defaults()->autoconfigure();
 
     $services->set(EmailGatewayMigration::class)
+        ->args([
+            service('database_connection'),
+        ])
+    ;
+
+    $services->set(MailerTransportMigration::class)
         ->args([
             service('database_connection'),
         ])
