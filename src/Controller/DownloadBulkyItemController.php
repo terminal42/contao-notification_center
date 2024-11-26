117b876e1fedd719d7f162e45ea2fae313713a4f
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-11-26 14:40:20 +0100

    Fix various stuff that failed in Contao 4.13

--- src/Controller/DownloadBulkyItemController.php
+++ src/Controller/DownloadBulkyItemController.php
@@ -7,8 +7,9 @@ namespace Terminal42\NotificationCenterBundle\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
-use Symfony\Component\HttpFoundation\UriSigner;
+use Symfony\Component\HttpFoundation\UriSigner as HttpFoundationUriSigner;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\UriSigner as HttpKernelUriSigner;
 use Symfony\Component\Routing\Attribute\Route;
 use Terminal42\NotificationCenterBundle\BulkyItem\BulkyItemStorage;
 
@@ -16,7 +17,7 @@ use Terminal42\NotificationCenterBundle\BulkyItem\BulkyItemStorage;
 class DownloadBulkyItemController
 {
     public function __construct(
-        private readonly UriSigner $uriSigner,
+        private readonly HttpFoundationUriSigner|HttpKernelUriSigner $uriSigner,
         private readonly BulkyItemStorage $bulkyItemStorage,
     ) {
     }
