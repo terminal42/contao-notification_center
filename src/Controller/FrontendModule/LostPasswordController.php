7620cb2ef7f72977950056284fc5b242a75c3d20
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2024-12-09 12:28:30 +0100

    Drop the ContentUrlGenerator dependency as we need to maintain compatibility with Contao 4.13

--- src/Controller/FrontendModule/LostPasswordController.php
+++ src/Controller/FrontendModule/LostPasswordController.php
@@ -6,7 +6,6 @@ namespace Terminal42\NotificationCenterBundle\Controller\FrontendModule;
 
 use Codefog\HasteBundle\Formatter;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
-use Contao\CoreBundle\Routing\ContentUrlGenerator;
 use Contao\Environment;
 use Contao\Idna;
 use Contao\ModuleModel;
@@ -22,7 +21,6 @@ use Terminal42\NotificationCenterBundle\Receipt\Receipt;
 class LostPasswordController extends LostPasswordModule
 {
     public function __construct(
-        private readonly ContentUrlGenerator $contentUrlGenerator,
         private readonly NotificationCenter $notificationCenter,
         private readonly Formatter $formatter,
     ) {
@@ -48,7 +46,7 @@ class LostPasswordController extends LostPasswordModule
 
         // Generate a custom target link URL, if any
         if ($this->nc_lost_password_jumpTo && null !== ($targetPage = PageModel::findPublishedById($this->nc_lost_password_jumpTo))) {
-            $tokens['link'] = $this->contentUrlGenerator->generate($targetPage, ['token' => $optInToken->getIdentifier()], UrlGeneratorInterface::ABSOLUTE_URL);
+            $tokens['link'] = $targetPage->getAbsoluteUrl(['token' => $optInToken->getIdentifier()]);
         } else {
             $tokens['link'] = Idna::decode(Environment::get('url')).Environment::get('requestUri').(str_contains((string) Environment::get('requestUri'), '?') ? '&' : '?').'token='.$optInToken->getIdentifier();
         }
