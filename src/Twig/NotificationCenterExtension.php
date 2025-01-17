567290b9adc7b12915acd29a134dacbe2a8bac4a
Andreas Schempp <andreas.schempp@terminal42.ch>
2025-01-17 10:54:37 +0100

    Add the format_bytes filter if it is not defined by Contao

--- src/Twig/NotificationCenterExtension.php
+++ src/Twig/NotificationCenterExtension.php
@@ -4,15 +4,37 @@ declare(strict_types=1);
 
 namespace Terminal42\NotificationCenterBundle\Twig;
 
+use Twig\Environment;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class NotificationCenterExtension extends AbstractExtension
 {
+    public function __construct(private readonly Environment $env)
+    {
+    }
+
     public function getFunctions(): array
     {
         return [
             new TwigFunction('notification_center_file_url', [NotificationCenterRuntime::class, 'fileUrl']),
+        ];
+    }
+
+    public function getFilters(): array
+    {
+        // Use the format_bytes filter in Contao 5
+        if (null !== $this->env->getFilter('format_bytes')) {
+            return [];
+        }
+
+        return [
+            new TwigFilter(
+                'format_bytes',
+                [NotificationCenterRuntime::class, 'formatBytes'],
+                ['is_safe' => ['html']],
+            )
         ];
     }
 }
