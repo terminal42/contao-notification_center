27e34ed00bfc98d535fedc6abbbb200d45c6cf15
Andreas Schempp <andreas.schempp@terminal42.ch>
2025-01-17 17:52:41 +0100

    Add the format_bytes filter for Contao 4.13 (#393)

--- src/Twig/NotificationCenterRuntime.php
+++ src/Twig/NotificationCenterRuntime.php
@@ -4,17 +4,31 @@ declare(strict_types=1);
 
 namespace Terminal42\NotificationCenterBundle\Twig;
 
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\System;
 use Terminal42\NotificationCenterBundle\BulkyItem\BulkyItemStorage;
 use Twig\Extension\RuntimeExtensionInterface;
 
 final class NotificationCenterRuntime implements RuntimeExtensionInterface
 {
-    public function __construct(private readonly BulkyItemStorage $bulkyItemStorage)
-    {
+    public function __construct(
+        private readonly ContaoFramework $framework,
+        private readonly BulkyItemStorage $bulkyItemStorage,
+    ) {
     }
 
     public function fileUrl(string $voucher, int|null $ttl = null): string
     {
         return $this->bulkyItemStorage->generatePublicUri($voucher, $ttl);
+    }
+
+    /**
+     * Convert a byte value into a human-readable format.
+     */
+    public function formatBytes(int $bytes, int $decimals = 1): string
+    {
+        $this->framework->initialize();
+
+        return System::getReadableSize($bytes, $decimals);
     }
 }
