b6c171edda722676b2279f955dd3395714220ae5
Andreas Schempp <andreas.schempp@terminal42.ch>
2025-01-17 14:07:59 +0100

    Only register if FormatterRuntime does not exist

--- src/Twig/NotificationCenterExtension.php
+++ src/Twig/NotificationCenterExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Terminal42\NotificationCenterBundle\Twig;
 
+use Contao\CoreBundle\Twig\Runtime\FormatterRuntime;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
@@ -19,6 +20,11 @@ class NotificationCenterExtension extends AbstractExtension
 
     public function getFilters(): array
     {
+        // This class only exists in Contao 5 and provides the format_bytes filter.
+        if (class_exists(FormatterRuntime::class)) {
+            return [];
+        }
+
         return [
             new TwigFilter(
                 'format_bytes',
