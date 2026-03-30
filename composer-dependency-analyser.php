85f612f221e0d36cbd7ffd9ad2f10643564aad79
Andreas Schempp <andreas.schempp@terminal42.ch>
2026-03-30 15:27:24 +0200

    LocaleSwitcher is available since Symfony 6.4
    see https://symfony.com/blog/new-in-symfony-6-1-locale-switcher

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -4,6 +4,5 @@ use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
-    ->ignoreErrorsOnPackage('symfony/translation', [ErrorType::SHADOW_DEPENDENCY])
     ->ignoreErrorsOnPackage('contao/newsletter-bundle', [ErrorType::DEV_DEPENDENCY_IN_PROD])
 ;
